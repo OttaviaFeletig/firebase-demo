6af91eae2d62d5e2dc9027de31835838d849fa72
OttaviaFeletig <ottafel@hotmail.it>
2022-02-16 11:34:32 +0100

    title changed

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   const { user, setUser } = useContext(AuthContext);
   return (
     <div>
-      <h2>Home Page</h2>
+      <h2>Hello Firebase</h2>
       {user && <h3>Welcome {user.email}</h3>}
     </div>
   );
